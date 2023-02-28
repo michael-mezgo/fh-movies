91b93965b9ea66f8497701b020b92ff8192460b1
michael-mezgo <58122000+michael-mezgo@users.noreply.github.com>
2023-02-28 13:14:33 +0100

    Wrote first unit test

--- src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,7 +1,8 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeControllerTest {
-
 }
--- src/test/java/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
@@ -1,7 +1,27 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
+    @Test
+    void movie_a_compared_to_movie_b_returns_negative_number()
+    {
+        //Given
+        List<Genre> genres = new ArrayList<>();
+        genres.add(Genre.ACTION);
+        Movie a = new Movie("a", "this is movie a", genres);
+        Movie b = new Movie("b", "this is movie b", genres);
 
+        //When
+        int actual = a.compareTo(b);
+
+        //Then
+        int expected = -1;
+        assertEquals(expected, actual);
+    }
 }
