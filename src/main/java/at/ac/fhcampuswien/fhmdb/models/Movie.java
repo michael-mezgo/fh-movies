80c2592841959e7aa90d8334ea62d0fd8d4499bf
Elias Meisl <112986140+elimei1@users.noreply.github.com>
2023-03-05 16:33:49 +0100

    new movies with descriptions

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -27,26 +27,77 @@ public class Movie implements Comparable<Movie>{
 
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
-        // TODO add some dummy data here
 
-        List<Genre> genresFirstMovie = new ArrayList<>();
-        genresFirstMovie.add(Genre.ADVENTURE);
-        genresFirstMovie.add(Genre.ACTION);
-        Movie myMovie = new Movie("title", "description", genresFirstMovie);
+        List<Genre> genresDarkKnight = new ArrayList<>();
+        genresDarkKnight.add(Genre.ACTION);
+        genresDarkKnight.add(Genre.CRIME);
+        genresDarkKnight.add(Genre.DRAMA);
+        Movie darkKnight = new Movie("The Dark Knight", "The Joker wreaks havoc on Gotham City while Batman tries to stop him in this dark and thrilling superhero film.", genresDarkKnight);
 
-        List<Genre> genresSecondMovie = new ArrayList<>();
-        genresSecondMovie.add(Genre.CRIME);
-        genresSecondMovie.add(Genre.DRAMA);
-        Movie myOtherMovie = new Movie("Group project", "technical difficulties", genresSecondMovie);
+        List<Genre> genresInception = new ArrayList<>();
+        genresInception.add(Genre.ACTION);
+        genresInception.add(Genre.ADVENTURE);
+        genresInception.add(Genre.SCIENCE_FICTION);
+        Movie inception = new Movie("Inception", "A thief who steals corporate secrets through dream-sharing technology is tasked with planting an idea in this mind-bending sci-fi thriller.", genresInception);
 
-        List<Genre> genresThirdMovie = new ArrayList<>();
-        genresThirdMovie.add(Genre.ROMANCE);
-        genresThirdMovie.add(Genre.FAMILY);
-        Movie myThirdMovie = new Movie("Networking technologies", "is fun", genresThirdMovie);
+        List<Genre> genresUp = new ArrayList<>();
+        genresUp.add(Genre.ANIMATION);
+        genresUp.add(Genre.ADVENTURE);
+        genresUp.add(Genre.COMEDY);
+        Movie up = new Movie("Up", "An elderly man and a young boy scout travel to South America in a house lifted by helium balloons in this heartwarming and adventurous animated film.", genresUp);
 
-        movies.add(myMovie);
-        movies.add(myOtherMovie);
-        movies.add(myThirdMovie);
+        List<Genre> genresAvengers = new ArrayList<>();
+        genresAvengers.add(Genre.ACTION);
+        genresAvengers.add(Genre.ADVENTURE);
+        genresAvengers.add(Genre.SCIENCE_FICTION);
+        Movie avengers = new Movie("The Avengers", "A group of superheroes band together to stop an alien invasion led by Thor's adopted brother, Loki, in this action-packed and epic comic book adaptation.", genresAvengers);
+
+        List<Genre> genresHungerGames = new ArrayList<>();
+        genresHungerGames.add(Genre.ACTION);
+        genresHungerGames.add(Genre.ADVENTURE);
+        genresHungerGames.add(Genre.SCIENCE_FICTION);
+        Movie hungerGames = new Movie("The Hunger Games", "A teenage girl volunteers to take her younger sister's place in a televised fight to the death in this thrilling and dystopian sci-fi adventure.", genresHungerGames);
+
+        List<Genre> genresFindingNemo = new ArrayList<>();
+        genresFindingNemo.add(Genre.ANIMATION);
+        genresFindingNemo.add(Genre.ADVENTURE);
+        genresFindingNemo.add(Genre.COMEDY);
+        Movie findingNemo = new Movie("Finding Nemo", "A clownfish sets out on a dangerous journey to find his son, who has been captured by divers, in this charming and visually stunning animated film.", genresFindingNemo);
+
+        List<Genre> genresArrival = new ArrayList<>();
+        genresArrival.add(Genre.SCIENCE_FICTION);
+        genresArrival.add(Genre.DRAMA);
+        Movie arrival = new Movie("Arrival", "When mysterious spacecraft touch down across the globe, a linguistics professor and her team race against time to interpret the aliens' language.", genresArrival);
+
+        List<Genre> genresGetOut = new ArrayList<>();
+        genresGetOut.add(Genre.HORROR);
+        genresGetOut.add(Genre.MYSTERY);
+        genresGetOut.add(Genre.THRILLER);
+        Movie getOut = new Movie("Get Out", "A young African-American man visits his white girlfriend's parents for the weekend, where his simmering uneasiness about their reception of him eventually reaches a boiling point in this chilling and socially relevant horror film.", genresGetOut);
+
+        List<Genre> genresMadMax = new ArrayList<>();
+        genresMadMax.add(Genre.ACTION);
+        genresMadMax.add(Genre.ADVENTURE);
+        genresMadMax.add(Genre.SCIENCE_FICTION);
+        Movie madMax = new Movie("Mad Max: Fury Road", "In a post-apocalyptic world, a woman rebels against a tyrannical ruler in search for her homeland with the aid of a group of female prisoners, a psychotic worshiper, and a drifter named Max in this exhilarating and intense action film.", genresMadMax);
+
+        List<Genre> genresTitans = new ArrayList<>();
+        genresTitans.add(Genre.BIOGRAPHY);
+        genresTitans.add(Genre.DRAMA);
+        genresTitans.add(Genre.SPORT);
+        Movie titans = new Movie("Remember the Titans", "The true story of a newly appointed African-American coach and his high school team on their first season as a racially integrated unit in this inspiring and emotional sports drama.", genresTitans);
+
+
+        movies.add(darkKnight);
+        movies.add(inception);
+        movies.add(up);
+        movies.add(avengers);
+        movies.add(hungerGames);
+        movies.add(arrival);
+        movies.add(getOut);
+        movies.add(findingNemo);
+        movies.add(madMax);
+        movies.add(titans);
 
         return movies;
     }
